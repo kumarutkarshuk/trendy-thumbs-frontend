81bad3e0605c858d0d39290afc4108f407e9fefb
kumarutkarshuk <kumarutkarshuk18@gmail.com>
2024-11-05 17:17:50 +0530

    misc: ui updated

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,18 +1,21 @@
+import Link from "next/link";
+import { RainbowButton } from "./ui/rainbow-button";
+import { portfolioUrl } from "@/lib/consts";
+
 export default function Footer() {
   return (
     <footer>
-      <div className="flex md:mt-20 mt-12 justify-center md:mb-6 mb-8">
-        <button className="inline-flex h-12 animate-shimmer items-center justify-center rounded-md border border-gray-900 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] px-6 font-medium text-slate-400 transition-colors">
-          <div className="flex gap-2">
-            <p>Last Analysis:</p>
-            <span className="text-[#2761D9]"> Nov 4, 2024</span>
-          </div>
-        </button>
-      </div>
-      <div className="flex justify-end md:mr-12 mr-6">
-        <p className="text-gray-500 text-xs md:text-base">
-          Developed by <span className="text-[#2761D9]">Utkarsh Kumar</span>
+      <div className="flex justify-end md:mr-12 mr-6 items-center gap-4 mt-20">
+        <p className="text-xs md:text-base">
+          Developed by
         </p>
+        <Link 
+        target="_blank"
+        rel="noopener noreferrer"
+        href={portfolioUrl}
+        >
+          <RainbowButton className="md:text-base text-xs px-4">Utkarsh Kumar</RainbowButton>
+        </Link>
       </div>
     </footer>
   );
--- components/powered-by.tsx
+++ components/powered-by.tsx
@@ -11,13 +11,26 @@ export default function PoweredBy() {
       className: "text-gray-200 text-xl md:text-5xl",
     },
     {
-      text: "Google Vision AI.",
+      text: "Google Vision AI",
       className: "text-[#2761D9] text-xl md:text-5xl",
     },
   ];
-  return(
-    <div className="flex justify-center md:pb-10 pb-4">
-        <TypewriterEffectSmooth words={words} cursorClassName="text-[#2761D9] h-6"/>
+  return (
+    <div className="flex flex-col items-center">
+      <div>
+        <TypewriterEffectSmooth
+          words={words}
+          cursorClassName="text-[#2761D9] h-6"
+        />
+      </div>
+      <div className="flex justify-center md:mb-12 mb-10">
+        <button className="inline-flex h-12 animate-shimmer items-center justify-center rounded-md border border-gray-900 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] md:px-6 px-4 font-medium text-slate-400 transition-colors text-xs md:text-base">
+          <div className="flex gap-2">
+            <p>Last Analysis:</p>
+            <span className="text-[#2761D9]"> Nov 4, 2024</span>
+          </div>
+        </button>
+      </div>
     </div>
-  )
+  );
 }
